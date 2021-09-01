e34698b9bd167595af3d2f0e1a398c7d5945e997
dnphu <phu.ngoc.dang@mgm-tp.com>
2021-09-01 22:40:15 +0700

    [Core] Update interface about Security doc

--- src/api/core/interfaces/document.interface.ts
+++ src/api/core/interfaces/document.interface.ts
@@ -5,7 +5,7 @@ export interface SwaggerDoc {
   servers?: ServerDoc[];
   tags?: TagDoc[];
   paths?: any;
-  components?: any;
+  components?: ComponentDoc;
 }
 
 export interface TagDoc {
@@ -51,3 +51,50 @@ export interface ExternalDoc {
   description?: string;
   url?: string;
 }
+
+export interface ComponentDoc {
+  schemas: any;
+  securitySchemes: {
+    [authKey: string]: SecurityDoc | BearerAuthDoc | ApiKeyDoc;
+  };
+}
+
+/**
+ * Attach link: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.0.1.md#securityRequirementObject
+ */
+export interface SecurityDoc {
+  type?: string;
+  description?: string;
+  name?: string;
+  in?: string;
+  flows?: OAuthFlows;
+  openIdConnectUrl?: any;
+  scheme?: string;
+  bearerFormat?: string;
+}
+
+export interface OAuthFlows {
+  implicit: OAuthFlow;
+  password: OAuthFlow;
+  clientCredentials: OAuthFlow;
+  authorizationCode: OAuthFlow;
+}
+
+export interface OAuthFlow {
+  authorizationUrl: string;
+  tokenUrl: string;
+  refreshUrl: string;
+  scopes: Record<string, string>;
+}
+
+export type BearerAuthDoc = {
+  type: 'http';
+  scheme: 'bearer';
+  bearerFormat: 'JWT';
+};
+
+export type ApiKeyDoc = {
+  type: 'apiKey';
+  name: 'api_key';
+  in: 'header';
+};
